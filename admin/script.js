cee9cdbda6c5d7321b6e3a1e3fcfe8746e6e7ddb
root <root@dsv.su.se>
2016-08-25 11:42:25 +0200

    General cleanup and more dnd implementation

--- admin/script.js
+++ admin/script.js
@@ -2,18 +2,18 @@ function make_sure(image) {
     return window.confirm("Är du säker på att du vill ta bort "+image+"?")
 }
 
-function dragstart_handler(ev) {
-    ev.dataTransfer.setData("text", ev.target.id)
-    ev.effectAllowed = "copyMove"
+function dragstart(ev) {
+    ev.dataTransfer.setData("draggedId", ev.target.id)
+    //ev.effectAllowed = "copyMove"
 }
 
-function dragend_handler(ev) {
+function dragend(ev) {
     ev.dataTransfer.clearData()
 }
 
 function drop(ev) {
     ev.preventDefault()
-    var droppedId = ev.dataTransfer.getData("text")
+    var droppedId = ev.dataTransfer.getData("draggedId")
     var copy = document.getElementById(droppedId).cloneNode(true)
     copy.id = ev.target.id + "_" + copy.id
     ev.target.appendChild(copy)
