aa5cc4a2f95fb36ec70b754272789e17eafb156d
root <root@dsv.su.se>
2017-05-12 23:44:06 +0200

    Updated the dump to the new format and trimmed it a bit.

--- include/database.sql
+++ include/database.sql
@@ -1,9 +1,3 @@
--- MySQL dump 10.13  Distrib 5.5.52, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: multiscreen
--- ------------------------------------------------------
--- Server version	5.5.52-0+deb8u1
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -14,6 +8,19 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `allowed_users`
+--
+
+DROP TABLE IF EXISTS `allowed_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `allowed_users` (
+  `user` varchar(64) NOT NULL,
+  PRIMARY KEY (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `show`
@@ -44,7 +51,7 @@ CREATE TABLE `show_image` (
   `show` bigint(20) NOT NULL,
   `seq` bigint(20) NOT NULL AUTO_INCREMENT,
   `endtime` bigint(20) DEFAULT NULL,
-PRIMARY KEY (`image`,`show`),
+  PRIMARY KEY (`image`,`show`),
   UNIQUE KEY `seq` (`seq`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -70,5 +77,3 @@ CREATE TABLE `slide` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-09-23 10:35:03
