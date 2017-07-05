10c2dbee64aab38f4d09e736495b33f552218c31
root <root@dsv.su.se>
2017-07-05 16:28:29 +0200

    Changing the primary key of the slide table

--- include/dbupdate-1.sql
+++ include/dbupdate-1.sql
@@ -1,2 +1,4 @@
 alter table `slide` add column `type` varchar(64);
 update `slide` set `type` = 'image' where `type` is NULL;
+alter table `slide` drop primary key;
+alter table `slide` add column `id` bigint(20) auto_increment primary key;
