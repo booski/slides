215c64b7276650a0f69375a8ab8aad0ceacf7625
root <root@dsv.su.se>
2017-07-26 10:33:37 +0200

    Fixing references to old primary keys

--- include/dbupdate-1.sql
+++ include/dbupdate-1.sql
@@ -2,3 +2,4 @@ alter table `slide` add column `type` varchar(64);
 update `slide` set `type` = 'image' where `type` is NULL;
 alter table `slide` drop primary key;
 alter table `slide` add column `id` bigint(20) auto_increment primary key;
+update `show_image` set `image` = (select `id` from `slide` where `image` = `show_image`.`image`);
