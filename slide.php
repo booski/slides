5868d941cb2eb4921090b032ce83d710102288e5
root <root@dsv.su.se>
2017-07-26 14:59:59 +0200

    Migrated most of the logic into functions.php

--- slide.php
+++ slide.php
@@ -2,44 +2,17 @@
 
 require_once './include/functions.php';
 
-$show = '';
-if(!isset($_GET['show'])) {
-    return false;
+$showid = '';
+if(isset($_GET['showid'])) {
+    $showid = $_GET['showid'];
 }
 
-$show = $_GET['show'];
-$dim = get_dimensions($show);
-
-$file = '';
-if(isset($_GET['name'])) {
-    $file = $_GET['name'];
+$slideid = '';
+if(isset($_GET['slideid'])) {
+    $slideid = $_GET['slideid'];
 }
 
-$im = '';
-if(!$file) {
-    
-    $im = create_image($dim['x'], $dim['y'], 'black', 'gray', $dim['x'].' x '.$dim['y']);
-    
-} else if(!file_exists($uldir.$file)) {
-
-    $im = create_image($dim['x'], $dim['y'], 'darkred', 'white', ":(\nNot found");
-    
-} else {
-
-    $file_scaled = $uldir.$dim['x'].'_'.$dim['y'].'_'.$file;
-
-    if(!file_exists($file_scaled)) {
-    
-        $im = new Imagick($uldir.$file);
-        $im->scaleImage($dim['x'], $dim['y'], true);
-        $im->writeImage($file_scaled);
-    
-    } else {
-        
-        $im = new Imagick($file_scaled);
-    }
-}
-
+$im = build_show_slide($showid, $slideid);
 header('Content-type:', $im->getImageMimeType());
 echo $im;
 exit(0);
