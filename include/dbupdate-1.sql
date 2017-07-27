81400ea611969429898be8f73901b6a7e8510d17
root <root@dsv.su.se>
2017-07-27 13:48:06 +0200

    Fixed an error in the script and added creation of the allowed_users table

--- include/dbupdate-1.sql
+++ include/dbupdate-1.sql
@@ -1,7 +1,8 @@
+create table if not exists `allowed_users` (`user` varchar(64) not null, primary key (`user`));
 alter table `slide` add column `type` varchar(64);
 update `slide` set `type` = 'image' where `type` is NULL;
 alter table `slide` drop primary key;
 alter table `slide` add column `id` bigint(20) auto_increment primary key;
-update `show_image` set `image` = (select `id` from `slide` where `image` = `show_image`.`image`);
+update `show_image` set `image` = (select `id` from `slide` where `name` = `show_image`.`image`);
 alter table `show_image` change column `image` `slide` bigint(20) not null;
 rename table `show_image` to `show_slide`;
