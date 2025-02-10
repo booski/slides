989c575ded1c2f446a14570fba3f6dee5abc3770
Erik Thuning <boooink@gmail.com>
2025-02-10 14:28:51 +0100

    Whitespace and dead code

--- picscript.js
+++ picscript.js
@@ -5,64 +5,59 @@ function init() {
     var timer = null
 
     function hideCursor() {
-	if(!list.contains(selector)) {
-	    list.add(selector)
-	}
+        if(!list.contains(selector)) {
+            list.add(selector)
+        }
     }
-    
+
     function mouseMove(event) {
-	if(timer) {
-	    window.clearTimeout(timer)
-	}
-	
-	if(list.contains(selector)) {
-	    list.remove(selector)
-	}
-	
-	timer = window.setTimeout(hideCursor, 1500)
+        if(timer) {
+            window.clearTimeout(timer)
+        }
+
+        if(list.contains(selector)) {
+            list.remove(selector)
+        }
+
+        timer = window.setTimeout(hideCursor, 1500)
     }
 
     function waitForNext(wait) {
-	if(wait) {
-	    window.setTimeout(getNext, wait*1000)
-	}
+        if(wait) {
+            window.setTimeout(getNext, wait*1000)
+        }
     }
 
     function getNext() {
-	var request = new XMLHttpRequest()
-	var showid = window.location.href.split('?')[1].split('=')[1]
-	request.open('GET', 'get.php?id=' + showid, true)
-	request.onreadystatechange = function getSlide() {
-	    if(request.readyState == 4) {
-		if(request.status == 200) {
-		    updateSlide(request.responseText)
-		} else {
-		    console.log("request failed, retrying")
-		    waitForNext(30)
-		}
-	    }
-	}
-	request.send()
+        var request = new XMLHttpRequest()
+        var showid = window.location.href.split('?')[1].split('=')[1]
+        request.open('GET', 'get.php?id=' + showid, true)
+        request.onreadystatechange = function getSlide() {
+            if(request.readyState == 4) {
+                if(request.status == 200) {
+                    updateSlide(request.responseText)
+                } else {
+                    console.log("request failed, retrying")
+                    waitForNext(30)
+                }
+            }
+        }
+        request.send()
     }
 
     function updateSlide(newpage) {
-	var temp = document.createElement('template')
-	temp.innerHTML = newpage
-	var newcontent = temp.content.querySelector('#content')
-	var newscript = temp.content.querySelector('script')
-	if(newcontent && newscript) {
-	    var oldcontent = container.querySelector('#content')
-	    oldcontent.replaceWith(newcontent)
-	    eval(newscript.innerHTML) // set new timeout
-	    waitForNext(timeout)
-	} else {
-	    waitForNext(30)
-	}
-	// container.removeChild(container.querySelector('#content'))
-	// container.removeChild(container.querySelector('script'))
-	// container.innerHTML = newpage
-	// eval(container.querySelector('script').innerHTML) // set new timeout
-	// waitForNext(timeout)
+        var temp = document.createElement('template')
+        temp.innerHTML = newpage
+        var newcontent = temp.content.querySelector('#content')
+        var newscript = temp.content.querySelector('script')
+        if(newcontent && newscript) {
+            var oldcontent = container.querySelector('#content')
+            oldcontent.replaceWith(newcontent)
+            eval(newscript.innerHTML) // set new timeout
+            waitForNext(timeout)
+        } else {
+            waitForNext(30)
+        }
     }
 
 
@@ -70,9 +65,9 @@ function init() {
 
     // timeout is defined externally
     if(timeout > 0) {
-	waitForNext(timeout)
+        waitForNext(timeout)
     }
-    
+
     document.getNext = getNext
 }
 
